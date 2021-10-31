64e23758579d66ca0e85088d5d9779289beab9a6
Anuj Sharma <anujsharmasppu@gmail.com>
2021-10-31 13:43:50 +0530

    add environment variables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { OrganisationModule } from 'src/organisation/organisation.module';
-import { OrganisationController } from '../organisation/organisation.controller';
-import { OrganisationService } from '../organisation/organisation.service';
+import { OrganisationModule } from '../organisation/organisation.module';
+import * as dotenv from 'dotenv';
+
+// Configure dotevn
+dotenv.config();
 
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://admin:5Dif3A4ruRUc1xfr@cluster0.buseu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.buseu.mongodb.net/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`,
     ),
     OrganisationModule,
   ],
-  controllers: [OrganisationController],
-  providers: [OrganisationService],
 })
 export class AppModule {}
